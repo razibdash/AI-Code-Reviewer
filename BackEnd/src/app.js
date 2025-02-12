50d3fc47182bfe2b97023536c7356c07ccf90c4a
avronilrajib <avronilrajib@gmail.com>
2025-02-12 23:30:14 +0600

    code editor and preview done

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const aiRoute=require('./routes/ai.route');
+const cors=require('cors');
 const app=express();
-
+app.use(cors());
 app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('Hello World');
--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,61 @@
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs";
+import Editor from "react-simple-code-editor";
+import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [code, setCode] = useState("function fetchData() {}");
+  const [response, setResponse] = useState("");
+  useEffect(() => {
+    prism.highlightAll();
+  });
+  const codeReview = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/get-review",
+        {
+          code,
+        }
+      );
+      setResponse(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
-      <div>
-        <h1>hello</h1>
-      </div>
+      <main>
+        <div className="left">
+          <div className="code">
+            {/* <pre>
+              <code className="language-javascript">
+                {`function fetchData() {}`}
+              </code>
+            </pre> */}
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript)
+              }
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                color: "white",
+              }}
+            />
+          </div>
+          <div className="reviewBtn">
+            <button onClick={codeReview}>Get Review</button>
+          </div>
+        </div>
+        <div className="right">
+          <div className="review"></div>
+        </div>
+      </main>
     </>
   );
 }
